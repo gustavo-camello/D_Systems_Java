48b0408428144928812af88575e24ede0850c778
Gustavo Camello <gucamello@gmail.com>
2022-03-26 17:32:58 +0000

    Add GUI to Service 2

--- src/main/java/ds/client/ControllerGUI.java
+++ src/main/java/ds/client/ControllerGUI.java
@@ -15,6 +15,7 @@ import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
 import ds.service1.Service1Grpc;
+import ds.service2.Service2Grpc;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
@@ -109,7 +110,32 @@ public class ControllerGUI implements ActionListener{
 		panel.setLayout(boxlayout);
 
 		return panel;
+	}
+	
+	private JPanel roomsLightSensorJPanel() {
 
+		JPanel panel = new JPanel();
+
+		BoxLayout boxlayout = new BoxLayout(panel, BoxLayout.X_AXIS);
+
+		JLabel label = new JLabel("Room Sensor Lights")	;
+		panel.add(label);
+		panel.add(Box.createRigidArea(new Dimension(10, 0)));
+		panel.add(Box.createRigidArea(new Dimension(10, 0)));
+		
+		JButton buttonOff = new JButton("Check Presence");
+		buttonOff.addActionListener(this);
+		panel.add(buttonOff);
+		panel.add(Box.createRigidArea(new Dimension(10, 0)));
+
+
+		responseField = new JTextField("", 10);
+		responseField .setEditable(false);
+		panel.add(responseField);
+
+		panel.setLayout(boxlayout);
+
+		return panel;
 	}
 	
 	public static void main(String[] args) {
@@ -138,6 +164,7 @@ public class ControllerGUI implements ActionListener{
 		panel.add( alarmJPanel() );
 		panel.add( lightSensorsJPanel() );
 		panel.add( panicAlarmJPanel() );
+		panel.add(roomsLightSensorJPanel());
 
 		// Set size for the frame
 		frame.setSize(300, 300);
@@ -152,12 +179,11 @@ public class ControllerGUI implements ActionListener{
 	public void actionPerformed(ActionEvent e) {
 		JButton button = (JButton)e.getSource();
 		String label = button.getActionCommand();  
-		
-		ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051).usePlaintext().build();
-		Service1Grpc.Service1BlockingStub blockingStub = Service1Grpc.newBlockingStub(channel);
 
 		if (label.equals("Alarm ON")) {
 			System.out.println("service 1 - Alarms is invoked to switch ON the Alarms...");
+			ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051).usePlaintext().build();
+			Service1Grpc.Service1BlockingStub blockingStub = Service1Grpc.newBlockingStub(channel);
 
 			//preparing message to send
 			ds.service1.RequestMessage request = ds.service1.RequestMessage.newBuilder().setMessage("Alarm ON").build();
@@ -169,6 +195,8 @@ public class ControllerGUI implements ActionListener{
 		
 		} else if (label.equals("Alarm OFF")) {
 			System.out.println("service 1 - Alarms is invoked to switch OFF the Alarms...");
+			ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051).usePlaintext().build();
+			Service1Grpc.Service1BlockingStub blockingStub = Service1Grpc.newBlockingStub(channel);
 
 			//preparing message to send
 			ds.service1.RequestMessage request = ds.service1.RequestMessage.newBuilder().setMessage("Alarm OFF").build();
@@ -180,6 +208,8 @@ public class ControllerGUI implements ActionListener{
 		
 		} else if (label.equals("LightSensors ON")) {
 			System.out.println("service 1 - Light Sensors is invoked to switch ON the Light Sensors...");
+			ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051).usePlaintext().build();
+			Service1Grpc.Service1BlockingStub blockingStub = Service1Grpc.newBlockingStub(channel);
 
 			//preparing message to send
 			ds.service1.RequestMessage request = ds.service1.RequestMessage.newBuilder().setMessage("LightSensor ON").build();
@@ -191,6 +221,8 @@ public class ControllerGUI implements ActionListener{
 			
 		} else if (label.equals("LightSensors OFF")) {
 			System.out.println("service 1 - Light Sensors is invoked to switch OFF the Light Sensors...");
+			ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051).usePlaintext().build();
+			Service1Grpc.Service1BlockingStub blockingStub = Service1Grpc.newBlockingStub(channel);
 
 			//preparing message to send
 			ds.service1.RequestMessage request = ds.service1.RequestMessage.newBuilder().setMessage("LightSensor OFF").build();
@@ -199,8 +231,11 @@ public class ControllerGUI implements ActionListener{
 			ds.service1.ResponseMessage response = blockingStub.activeLightSensors(request);
 
 			responseField.setText( response.getMessage() );
+			
 		} else if (label.equals("Active Panic Alarm")) {
 			System.out.println("service 1 - Active Panic Alarm is invoked to switch ON the Active Panic Alarm...");
+			ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051).usePlaintext().build();
+			Service1Grpc.Service1BlockingStub blockingStub = Service1Grpc.newBlockingStub(channel);
 
 			//preparing message to send
 			ds.service1.RequestMessage request = ds.service1.RequestMessage.newBuilder().setMessage("Active Panic Alarm").build();
@@ -209,6 +244,30 @@ public class ControllerGUI implements ActionListener{
 			ds.service1.ResponseMessage response = blockingStub.activePanicAlarm(request);
 
 			responseField.setText( response.getMessage() );
+			
+		} else if (label.equals("Check Presence")) {
+			System.out.println("service 2 - Check the presence in a room and switch on or off the lights");
+			ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051).usePlaintext().build();
+			Service2Grpc.Service2BlockingStub blockingStub = Service2Grpc.newBlockingStub(channel);
+
+			//preparing message to send
+			ds.service2.RequestMessage request = ds.service2.RequestMessage.newBuilder().setText("Sensor Reading...").build();
+
+			//retreving reply from service
+			ds.service2.ResponseMessage response = blockingStub.checkPresence(request);
+			
+			// TODO: NEEDS A WAY TO CONVERT THE PRESENCE RESPONSE TO A BOOLEAN, THAN UNCOMMENT THE CODE BELOW
+			
+//			// Switch the lights on response
+//			ds.service2.hasPrecense request2 = ds.service2.hasPrecense.newBuilder().setPresence(Boolean.valueOf(response)).build();
+//			ds.service2.ResponseMessage response2 = blockingStub.turnLightsOn(request2);
+//			
+//			// Switch the lights off when sensor is not reading a presence
+//			ds.service2.hasPrecense request3 = ds.service2.hasPrecense.newBuilder().setPresence(Boolean.valueOf(response)).build();
+//			ds.service2.ResponseMessage response3 = blockingStub.turnLightsOff(request2);
+			
+
+			responseField.setText( response.getText() );
 		}
 	}
 }
